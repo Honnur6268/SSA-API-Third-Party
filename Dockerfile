798df25ff7fc2d4fb1277b88b4a2464117dbab8c
Honnur Ali <honnur@neokred.tech>
2024-05-21 11:02:22 +0530

    updated code and pushed

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:17
 MAINTAINER Honnur
-COPY  target/ssa-api.jar  ssa-api.jar
-EXPOSE 7000
-ENTRYPOINT ["java", "-jar", "/ssa-api.jar"]
+COPY  target/ssa-api.jar  /usr/app/
+WORKDIR /usr/app/
+ENTRYPOINT ["java", "-jar", "ssa-api.jar"]
+EXPOSE 8080
--- src/main/java/com/honnur/ssa/restcontroller/SSACitizenDetialsRestController.java
+++ src/main/java/com/honnur/ssa/restcontroller/SSACitizenDetialsRestController.java
@@ -1,9 +1,12 @@
 package com.honnur.ssa.restcontroller;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +35,10 @@ public class SSACitizenDetialsRestController {
 			error.put("errorMessage", "Invalid SSN: " + ssnDetails.getSsn());
 			return "Invalid SSN";
 		}
+	}
+	
+	@GetMapping("/cities")
+	public List<String> getAllSSNDetails(){
+		return Arrays.asList("New Jersey","Ohio","Texas","California","Rhode Island","Kentucky");
 	}
 }
